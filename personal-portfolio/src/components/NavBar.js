374c7330892806bf693fb9577cd64d2198847205
Aroush Qureshi <aroushq1@gmail.com>
2024-12-06 15:20:06 -0700

    Working on Navigation Bar

---
+++ personal-portfolio/src/components/NavBar.js
@@ -0,0 +1,26 @@
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+
+export const NavBar = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <NavBar.Brand href ="#home">React-Bootstrap</NavBar.Brand>
+                <NavBar.Toggle aria-controls="basic-navbar-nav" />
+                <NavBar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href ="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title ="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider/>
+                            <NavDropdown.Item href="#action/3.4">Separated Link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </NavBar.Collapse>
+            </Container>
+            
+        </Navbar>
+    )
+}
