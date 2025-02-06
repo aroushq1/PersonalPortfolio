770c874cfc6a45edcb0d2fd8562cb402e895b921
Aroush Qureshi <aroushq1@gmail.com>
2025-02-06 16:35:04 -0700

    update

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -27,8 +27,8 @@ const Home = () => {
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <a
-                href="/assets/resume/AroushQ_RESUME2024.pdf"  // Path to your CV file in the public directory
-                download="My_CV.pdf"  
+                href=""  // Path to your CV file in the public directory
+                download=""  
               >
                 <Button
                   variant="outline"
